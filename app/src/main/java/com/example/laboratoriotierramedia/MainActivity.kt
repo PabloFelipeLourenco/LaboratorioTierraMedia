133adbd53d6229fd8d43220f650e4bd51b3706db
PabloFelipeLourenco <2905247@alu.murciaeduca.es>
2026-09-23 15:37:12 +0200

    Commit: Hemos realizado los requisitos lógicos.
    Al pulsar el botón, el texto del saludo cambia y el contador suma 1 cada vez que se pulsa el botón.
    
    Los logs de depuración están incluidos en las funcione de ciclo de vida y cada log simula una fase del viaje del anillo.

--- app/src/main/java/com/example/laboratoriotierramedia/MainActivity.kt
+++ app/src/main/java/com/example/laboratoriotierramedia/MainActivity.kt
@@ -5,10 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var contador = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("TierraMedia", "El viaje comienza en Rivendell")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +21,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val boton = findViewById<Button>(R.id.button)
+        val saludo = findViewById<TextView>(R.id.textView2)
+        val contadorTexto = findViewById<TextView>(R.id.textView)
+        boton.setOnClickListener {
+            contador++
+            saludo.text = "¡La Comunidad del Anillo ha partido hacia Mordor!"
+            contadorTexto.text = "Miembros reunidos: $contador"
+            Log.d("TierraMedia", "Un nuevo miembro se une a la Comunidad")
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("TierraMedia", "El Anillo abandona Rivendell y comienza su viaje")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("TierraMedia", "La Comunidad continúa su viaje hacia Mordor")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d("TierraMedia", "La Comunidad hace una pausa en su camino")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("TierraMedia", "La Comunidad abandona temporalmente la escena")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TierraMedia", "El viaje del Anillo llega a su fin")
     }
 }
